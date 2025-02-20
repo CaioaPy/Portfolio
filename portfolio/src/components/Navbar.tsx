4a5273afe29512f19d06614d4212f4bdb4909b3e
yatomikazuchi <caioevilabner@gmail.com>
2025-02-20 20:34:29 -0300

    navbar style updated

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <div className="navbar">
-            <Link to="/Projects"><button>Projects</button></Link>
-            <Link to="/"><button>Home</button></Link>
-            <Link to="/Experience"><button>Experience</button></Link>
+            <div className="navContainer">
+                <Link to="/Projects"><button>Projects</button></Link>
+                <Link to="/"><button>Home</button></Link>
+                <Link to="/Experience"><button>Experience</button></Link>
             </div>
+        </div>
     )
 }
 
