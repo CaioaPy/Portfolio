b33aa84812d2aaf9ec1769587d9f5ff4009ec317
yatomikazuchi <caioevilabner@gmail.com>
2025-02-21 08:57:43 -0300

    better css files and colors palette change

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -4,13 +4,16 @@ import './Home.css';
 
 function Home() {
     return (
-        <>
+        <div className="containerHome">
             <Navbar />
             <div className="startContainerHome">
                 <div className="imgIntro"><img src="https://pe-images.s3.amazonaws.com/basics/cc/image-size-resolution/resize-images-for-print/image-cropped-8x10.jpg" alt="ee"></img></div>
                 <h1 className="introText">Helloo! I'm Caio and this is my portfolio!</h1>
             </div>
-        </>
+            <div className="secondContainerHome">
+                <h1>section</h1>
+            </div>
+        </div>
     );
 }
 
