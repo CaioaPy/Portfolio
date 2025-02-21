da194121648462cc4e72ffc1a9b2d0c448d82af2
yatomikazuchi <caioevilabner@gmail.com>
2025-02-21 07:52:20 -0300

    image template for home

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -6,7 +6,8 @@ function Home() {
     return (
         <>
             <Navbar />
-            <div className="start-container">
+            <div className="startContainerHome">
+                <div className="imgIntro"><img src="https://pe-images.s3.amazonaws.com/basics/cc/image-size-resolution/resize-images-for-print/image-cropped-8x10.jpg" alt="ee"></img></div>
                 <h1 className="introText">Helloo! I'm Caio and this is my portfolio!</h1>
             </div>
         </>
