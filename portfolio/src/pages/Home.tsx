e2724db304cca8aee2af15913ec3167e421b02b3
yatomikazuchi <caioevilabner@gmail.com>
2025-02-23 10:37:41 -0300

    layout updated (img>title>subtitle>paragraph)

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -10,9 +10,9 @@ function Home() {
                 <div className="imgIntro"><img src="https://pe-images.s3.amazonaws.com/basics/cc/image-size-resolution/resize-images-for-print/image-cropped-8x10.jpg" alt="ee"></img></div>
                 <div className="introTitle">Caio Abner
                     <div className="introSubtitle">Full Stack Developer</div>
+                    <div className="introText">Helloo! I'm Caio and this is my portfolio! Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!</div>
                 </div>
                 <div className="introIcons"><button></button><button></button><button></button><button></button><button></button><button></button></div>
-                <div className="introText">Helloo! I'm Caio and this is my portfolio! Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!Helloo! I'm Caio and this is my portfolio!</div>
             </div>
             <div className="secondContainerHome">
                 <h1>section</h1>
