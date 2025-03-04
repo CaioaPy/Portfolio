bd9b5bf8174ca5e41a2cd20d912b9b590590de86
yatomikazuchi <caioevilabner@gmail.com>
2025-03-04 19:28:17 -0300

    started contact page

--- portfolio/src/pages/Contact.tsx
+++ portfolio/src/pages/Contact.tsx
@@ -1,11 +1,18 @@
 import Navbar from "../components/Navbar";
 import './Contact.css';
+import Background from "../components/Background";
 
 function Contact() {
     return (
         <div className="start-container">
-        <Navbar />
-        <h1>Contact</h1>
+            <Background />
+            <Navbar />
+            <div className="mainContent">
+                <h1 className="titleH1">Let's Get In Touch</h1>
+                <h2 className="contactText">I'm always open to new opportunities and exciting collaborations!
+                    Whether you have a question or want to work together on a project, feel free to reach out.
+                    My inbox is always open, and I'll do my best to respond as soon as possible.</h2>
+            </div>
         </div>
     );
 }
