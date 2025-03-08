6a39ac511be96688735d47f7c89107cdecc53c7a
yatomikazuchi <caioevilabner@gmail.com>
2025-03-07 22:53:03 -0300

    contact page layout update

--- portfolio/src/pages/Contact.tsx
+++ portfolio/src/pages/Contact.tsx
@@ -8,7 +8,8 @@ function Contact() {
             <Background />
             <Navbar />
             <div className="mainContent">
-                <h1 className="titleH1">Let's Get In Touch</h1>
+                <h1 className="mainH1">Contact</h1>
+                <h1 className="titleH1">Get In Touch</h1>
                 <h2 className="contactText">I'm always open to new opportunities and exciting collaborations!
                     Whether you have a question or want to work together on a project, feel free to reach out.
                     My inbox is always open, and I'll do my best to respond as soon as possible.</h2>
