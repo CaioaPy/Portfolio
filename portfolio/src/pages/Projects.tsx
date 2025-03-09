9e7085cc6023f7d640595bfd1d05030ec87bb1a5
yatomikazuchi <caioevilabner@gmail.com>
2025-03-08 22:46:38 -0300

    resized contact icons and started projects page layout

--- portfolio/src/pages/Projects.tsx
+++ portfolio/src/pages/Projects.tsx
@@ -1,11 +1,19 @@
 import Navbar from "../components/Navbar";
+import Background from '../components/Background';
 import './Projects.css';
 
 function Projects() {
     return (
         <div className="start-container">
-        <Navbar />
-        <h1>Projects</h1>
+            <Navbar />
+            <Background />
+            <div className="projectsContainer">
+                <h1>My Projects</h1>
+                <div className="projectCard">
+                    <div className="projectTitle">Project 1</div>
+                    <div className="projectDescription">Description 1</div>
+                </div>
+            </div>
         </div>
     );
 }
