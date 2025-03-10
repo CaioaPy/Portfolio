af0fd523e184cc205b08afeff6c98794419e817c
yatomikazuchi <caioevilabner@gmail.com>
2025-03-10 19:17:30 -0300

    Fufu and PDFast projects added, minor changes to Projects card layout

--- portfolio/src/pages/Projects.tsx
+++ portfolio/src/pages/Projects.tsx
@@ -3,6 +3,8 @@ import Background from '../components/Background';
 import './Projects.css';
 import github from '../assets/imgs/github.png';
 import external from '../assets/imgs/externalLink.png';
+import Fufu from '../assets/imgs/projects/Fufu.png';
+import PDFast from '../assets/imgs/projects/PDFast.png';
 
 function Projects() {
     return (
@@ -13,18 +15,37 @@ function Projects() {
                 <h1 className="title">My Projects</h1>
                 <div className="projectsContainer">
                     <div className="projectCard">
-                        <div className="projectTitle">Project 1</div>
-                        <div className="projectDescription">DASDLKAJDS LASKJD ASLK FJADSLF JDSLFJFEALFD AJS DLKSAJD LSAKJ DSALKJDF ALKDJWA LKDJSA LKDJSA</div>
+                        <div className="projectTitle">PDFast</div>
+                        <div className="projectDescription">PDFast is a tool designed to extract, summarize, and translate the content of PDF files quickly
+                            and conveniently. With an intuitive interface, the application allows you to save time by transforming lengthy documents into clear
+                            and accessible summaries in Portuguese.</div>
                         <div className="images">
-                            <img src="" className="projectImage" alt="Project Thumbnail"></img>
+                            <img src={PDFast} className="projectImage" alt="Project Thumbnail"></img>
                         </div>
                         <div className="projectTech">
                             <h1>Technologies Used:</h1> 
-                            <p>HTML, CSS, JavaScript</p>
+                            <p>Python, Tkinter, Cohere AI</p>
                         </div>
                         <div className="projectIcons">
-                            <img src={github} className="projectIcon" alt="GitHub Icon"></img>
-                            <img src={external} className="projectIcon" alt="External Link Icon"></img>
+                            <a href="https://github.com/CaioaPy/Fufu/tree/master" target="_blank"><img src={github} className="projectIcon" alt="GitHub Icon"></img></a>
+                            <a href="fufu-pi.vercel.app" target="_blank"><img src={external} className="projectIcon" alt="External Link Icon"></img></a>
+                        </div>
+                    </div>
+                    <div className="projectCard">
+                        <div className="projectTitle">Fufu</div>
+                        <div className="projectDescription">Fufu is a dynamic web platform designed to enhance your RPG experiences with ease and creativity.
+                            Whether you're rolling dice, generating unique characters, or crafting entire adventures, Fufu provides the tools needed to elevate
+                            your role-playing sessions.</div>
+                        <div className="images">
+                            <img src={Fufu} className="projectImage" alt="Project Thumbnail"></img>
+                        </div>
+                        <div className="projectTech">
+                            <h1>Technologies Used:</h1> 
+                            <p>.NET, C#, ASP.NET MVC,React, JavaScript, HTML, CSS</p>
+                        </div>
+                        <div className="projectIcons">
+                            <a href="https://github.com/CaioaPy/Fufu/tree/master" target="_blank"><img src={github} className="projectIcon" alt="GitHub Icon"></img></a>
+                            <a href="fufu-pi.vercel.app" target="_blank"><img src={external} className="projectIcon" alt="External Link Icon"></img></a>
                         </div>
                     </div>
                 </div>
