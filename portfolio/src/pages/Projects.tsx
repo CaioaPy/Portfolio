d3c800a315e0901846e1c736e0362c4c18c16921
yatomikazuchi <caioevilabner@gmail.com>
2025-03-11 21:27:32 -0300

    minor change to home margin, major change to projects page (removed projects container and modified project cards layout)

--- portfolio/src/pages/Projects.tsx
+++ portfolio/src/pages/Projects.tsx
@@ -12,41 +12,39 @@ function Projects() {
             <Navbar />
             <Background />
             <div className="pageContainer">
-                <h1 className="title">My Projects</h1>
-                <div className="projectsContainer">
-                    <div className="projectCard">
-                        <div className="projectTitle">PDFast</div>
-                        <div className="projectDescription">PDFast is a tool designed to extract, summarize, and translate the content of PDF files quickly
-                            and conveniently. With an intuitive interface, the application allows you to save time by transforming lengthy documents into clear
-                            and accessible summaries in Portuguese.</div>
-                        <div className="images">
-                            <img src={PDFast} className="projectImage" alt="Project Thumbnail"></img>
-                        </div>
-                        <div className="projectTech">
-                            <h1>Technologies Used:</h1> 
-                            <p>Python, Tkinter, Cohere AI</p>
-                        </div>
-                        <div className="projectIcons">
-                            <a href="https://github.com/CaioaPy/Fufu/tree/master" target="_blank"><img src={github} className="projectIcon" alt="GitHub Icon"></img></a>
-                            <a href="fufu-pi.vercel.app" target="_blank"><img src={external} className="projectIcon" alt="External Link Icon"></img></a>
-                        </div>
+            <h1 className="title">My Projects</h1>
+    
+                <div className="projectCard">
+                    <div className="projectTitle">PDFast</div>
+                    <div className="projectDescription">PDFast is a tool designed to extract, summarize, and translate the content of PDF files quickly
+                        and conveniently. With an intuitive interface, the application allows you to save time by transforming lengthy documents into clear
+                        and accessible summaries in Portuguese.</div>
+                    <div className="images">
+                        <img src={PDFast} className="projectImage" alt="Project Thumbnail"></img>
                     </div>
-                    <div className="projectCard">
-                        <div className="projectTitle">Fufu</div>
-                        <div className="projectDescription">Fufu is a dynamic web platform designed to enhance your RPG experiences with ease and creativity.
-                            Whether you're rolling dice, generating unique characters, or crafting entire adventures, Fufu provides the tools needed to elevate
-                            your role-playing sessions.</div>
-                        <div className="images">
-                            <img src={Fufu} className="projectImage" alt="Project Thumbnail"></img>
-                        </div>
-                        <div className="projectTech">
-                            <h1>Technologies Used:</h1> 
-                            <p>.NET, C#, ASP.NET MVC,React, JavaScript, HTML, CSS</p>
-                        </div>
-                        <div className="projectIcons">
-                            <a href="https://github.com/CaioaPy/Fufu/tree/master" target="_blank"><img src={github} className="projectIcon" alt="GitHub Icon"></img></a>
-                            <a href="fufu-pi.vercel.app" target="_blank"><img src={external} className="projectIcon" alt="External Link Icon"></img></a>
-                        </div>
+                    <div className="projectTech">
+                        <h1>Technologies Used:</h1> 
+                        <p>Python, Tkinter, Cohere AI</p>
+                    </div>
+                    <div className="projectIcons" style={{marginLeft: "87%"}}>
+                        <a href="https://github.com/CaioaPy/PDFast" target="_blank"><img src={github} className="projectIcon" alt="GitHub Icon"></img></a>
+                    </div>
+                </div>
+                <div className="projectCard">
+                    <div className="projectTitle">Fufu</div>
+                    <div className="projectDescription">Fufu is a dynamic web platform designed to enhance your RPG experiences with ease and creativity.
+                        Whether you're rolling dice, generating unique characters, or crafting entire adventures, Fufu provides the tools needed to elevate
+                        your role-playing sessions.</div>
+                    <div className="images">
+                        <img src={Fufu} className="projectImage" alt="Project Thumbnail"></img>
+                    </div>
+                    <div className="projectTech">
+                        <h1>Technologies Used:</h1> 
+                        <p>.NET, C#, ASP.NET MVC,React, JavaScript, HTML, CSS</p>
+                    </div>
+                    <div className="projectIcons">
+                        <a href="https://github.com/CaioaPy/Fufu/tree/master" target="_blank"><img src={github} className="projectIcon" alt="GitHub Icon"></img></a>
+                        <a href="fufu-pi.vercel.app" target="_blank"><img src={external} className="projectIcon" alt="External Link Icon"></img></a>
                     </div>
                 </div>
             </div>
